fe683dc2a8332abdb8de7a26cc5df960b28d4c40
flariet <flariet@github.com>
2019-12-26 15:28:57 +0800

    retrieve two_char

--- R/data.R
+++ R/data.R
@@ -12,3 +12,22 @@
 #' }
 "drugnames"
 
+#' Frequency, PMI and BE dataframe of Chinese words wtih two characters.
+#'
+#' This dataframe contains Chinese Word with two characters with its branch entropy and pmi value
+#'
+#' @format A data frame with 1 variables:
+#' \describe{
+#' \item{\code{character}}{chinese word with two characters.}
+#' \item{\code{freq}}{Occurence times of target word in real world.}
+#' \item{\code{pct}}{Frequency of target word in real world.}
+#' \item{\code{A}}{entropy of first character in target word.}
+#' \item{\code{B}}{entropy of second character in target word.}
+#' \item{\code{real_log}}{pmi value of target word.}
+#' }
+#' @examples
+#' \dontrun{
+#'  two_char
+#'  }
+"two_char"
+
